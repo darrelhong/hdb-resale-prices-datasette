abcac16d92df6648f3a2db8dd3ddd9814729e7c5
darrelhong <darrel.hong@gmail.com>
2023-03-11 01:06:41 +0800

    feat: exit if no new data

--- download.sh
+++ download.sh
@@ -1,5 +1,12 @@
 mkdir -p ~/downloads
 mkdir -p data/datasets
+
+CURR_CSV_MD5=$(md5sum ~/downloads/datasets.zip | awk '{ print $1 }')
 conditional-get --etags ~/downloads/etags.json  --output ~/downloads/datasets.zip 'https://storage.data.gov.sg/resale-flat-prices/resale-flat-prices.zip'
+NEW_CSV_MD5=$(md5sum ~/downloads/datasets.zip | awk '{ print $1 }')
+
+if [ "$CURR_CSV_MD5" = "$NEW_CSV_MD5" ]; then
+    exit 1
+fi
 
 unzip ~/downloads/datasets.zip -d data/datasets
