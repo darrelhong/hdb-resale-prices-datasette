ebc64ad50c834a47ad76522271677ca1a1cbb551
darrelhong <darrel.hong@gmail.com>
2025-05-24 13:22:44 +0800

    try add index

--- build_db.sh
+++ build_db.sh
@@ -8,6 +8,9 @@ sqlite-utils create-index app/resale.db resale_prices month
 sqlite-utils create-index app/resale.db resale_prices town
 sqlite-utils create-index app/resale.db resale_prices flat_type
 sqlite-utils create-index app/resale.db resale_prices flat_model
+sqlite-utils create-index app/resale.db resale_prices block
+sqlite-utils create-index app/resale.db resale_prices street_name
+
 
 # build _footer.html
 FORMATTED_DATE=$(date +"%-d %b %Y")
